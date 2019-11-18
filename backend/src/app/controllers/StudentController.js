c2f22b7ef0c9e9afc8e9f729845f513fa8f157ed
Júnior de Oliveira <ellismarjr@hotmail.com>
2019-11-18 15:43:52 -0300

    feat: Admin users can remove a student

--- backend/src/app/controllers/StudentController.js
+++ backend/src/app/controllers/StudentController.js
@@ -75,6 +75,17 @@ class StudentController {
 
     return res.json({ id, name, age, weight, height });
   }
+
+  async delete(req, res) {
+    const student = await Student.findByPk(req.params.id);
+    if (!student) {
+      return res.status(400).json({ error: 'Student not exists!' });
+    }
+
+    await student.destroy();
+
+    return res.json(student);
+  }
 }
 
 export default new StudentController();
