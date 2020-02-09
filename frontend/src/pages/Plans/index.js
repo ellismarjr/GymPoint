1c22960faa9baf5a0efcd3f2b3c9a811cda28415
Júnior de Oliveira <ellismarjr@hotmail.com>
2020-02-09 14:55:00 -0300

    feat: Admin can delete plan

--- frontend/src/pages/Plans/index.js
+++ frontend/src/pages/Plans/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-
+import { confirmAlert, onClose } from 'react-confirm-alert';
 import api from '~/services/api';
 import history from '~/services/history';
 import { formatPrice } from '~/util/format';
@@ -32,6 +32,41 @@ export default function Plans() {
     history.push(`/plan/${id}/edit`);
   }
 
+  async function handleDeletePlan(id) {
+    try {
+      await api.delete(`plans/${id}`);
+    } catch (err) {
+      toast.error('Erro ao excluir plano!');
+    }
+
+    toast.success('Plano excluído com sucesso!');
+    loadPlans(1);
+  }
+
+  function handleClose() {
+    confirmAlert({ onClose });
+  }
+
+  function handleConfirm(id, title) {
+    confirmAlert(
+      {
+        title: 'Exclusão de Plano',
+        message: `Deseja realmente excluir o plano ${title}`,
+        buttons: [
+          {
+            label: 'Sim',
+            onClick: () => handleDeletePlan(id),
+          },
+          {
+            label: 'Não',
+            onClick: () => handleClose(),
+          },
+        ],
+      },
+      { onClickOutside: true }
+    );
+  }
+
   return (
     <Container>
       <header>
@@ -62,7 +97,12 @@ export default function Plans() {
                   <button type="button" onClick={() => handleEditPlan(plan.id)}>
                     Editar
                   </button>
-                  <button type="button">Excluir</button>
+                  <button
+                    type="button"
+                    onClick={() => handleConfirm(plan.id, plan.title)}
+                  >
+                    Excluir
+                  </button>
                 </td>
               </tr>
             ))}
